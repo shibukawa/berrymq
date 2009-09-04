d8a5f37bdcbf619c32810628ae444359726f3f6f
shibu <shibu@shibukawa-yoshiki-no-macbook.local>
2009-09-04 09:28:59 +0900

    python2&3: add update setup.py (use setuptools)

---
+++ python3/setup.py
@@ -0,0 +1,25 @@
+import sys
+from setuptools import setup, find_packages 
+
+__version__ = open("../VERSION").read().strip()
+
+setup(name = 'berrymq',
+      version = __version__,
+      author = 'SHIBUKAWA Yoshiki',
+      author_email = 'yoshiki at shibu.jp',
+      url = 'http://bitbucket.org/shibu/berrymq/',
+      description = 'Small message queue system for building applications',
+      long_description = ('Programmer friendly message queue module. '
+                        'It supports push/pull API, thread pool'),
+      keywords = "python ruby message queue mq",
+      classifiers = [
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python"
+      ],
+      license = "MIT License",
+      packages = find_packages(),
+      install_requires = [
+      ])
+
